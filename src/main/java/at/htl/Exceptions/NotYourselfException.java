fd8e6b3d494b8232249835e7365a82f117aa81e1
max <lettnermaximilian2008@gmail.com>
2025-11-12 20:16:13 +0100

    added most funcionality for wandering trader

---
+++ src/main/java/at/htl/Exceptions/NotYourselfException.java
@@ -0,0 +1,7 @@
+package at.htl.Exceptions;
+
+public class NotYourselfException extends RuntimeException {
+    public NotYourselfException(String message) {
+        super(message);
+    }
+}
--- src/main/java/at/htl/WanderingTrader.java
+++ src/main/java/at/htl/WanderingTrader.java
@@ -1,11 +1,37 @@
 package at.htl;
 
+import at.htl.Exceptions.AlreadyLastTimeException;
+import at.htl.Exceptions.NotYourselfException;
+
 public class WanderingTrader extends Player {
-    private Player _oldStuffed;
+    private Player _stuffed;
+    private Player _sleptWith;
 
     WanderingTrader(int playerCount, String name) {
-        _oldStuffed = null;
+        _stuffed = null;
+        _sleptWith = null;
 
         super(playerCount, name);
+    }
+
+    public void stuff(Player player) {
+        if (_stuffed == player) {
+            throw new AlreadyLastTimeException("You can't stuff the same person two times in a row!");
+        }
+        if (player == this) {
+            throw new NotYourselfException("You can't stuff yourself!");
+        }
+        _stuffed = player;
+    }
+
+    public void sleepWith(Player player) {
+        //if (_sleptWith == _werewolf.getVictim()) {}
+        if (_sleptWith == player) {
+            throw new AlreadyLastTimeException("You can't sleep with the same person two times in a row!");
+        }
+        if (player == this) {
+            throw new NotYourselfException("You can't sleep with yourself!");
+        }
+        _sleptWith = player;
     }
 }
