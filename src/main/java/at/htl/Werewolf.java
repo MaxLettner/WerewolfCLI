530456fefafebbc1e3b7c53a92988248b65ec292
max <lettnermaximilian2008@gmail.com>
2025-11-12 17:43:39 +0100

    added not finished code to werewolf for attacking

--- src/main/java/at/htl/Werewolf.java
+++ src/main/java/at/htl/Werewolf.java
@@ -4,4 +4,15 @@ public class Werewolf extends Player {
     Werewolf(int playerCount, String name) {
         super(playerCount, name);
     }
+
+    public void attack(Player player) {
+        if(_isInLoveWith == player) {
+            //throw new FriendlyFireException();
+        }
+        if(!player._isAlive) {
+            //throw new TargetDeadException();
+        }
+
+        player.setHealth(player.getHealth() - 1);
+    }
 }
